ce8707dfe2bae5573cb7b3223fd4f89d8e9357eb
unknown <shenyanzheng00@gmail.com>
2022-07-09 11:52:54 -0400

    commit files

--- MainGame.py
+++ MainGame.py
@@ -1,2 +1,6 @@
+import json
+
 if __name__ == "__main__":
-    print("111")
+    with open('questions_data.json') as json_file:
+        data = json.load(json_file)
+        print(data)
--- QuestionGenerator.py
+++ QuestionGenerator.py
@@ -4,20 +4,24 @@ import json
 
 def load_questions():
     file_1 = pd.read_excel('Trivia-Printable.xlsx', "Trivia")
-    questions = []
-    category = {}
+    questions = {"questions":[]}
+    # category = {}
     for row_index, row in file_1.iterrows():
-        if file_1.iloc[row_index, 0] not in category:
-            category[file_1.iloc[row_index, 0]] = []
-        questions.append({})
-        questions[row_index]["category"] = file_1.iloc[row_index, 0]
-        questions[row_index]["question"] = file_1.iloc[row_index, 1]
-        questions[row_index]["correct answer"] = file_1.iloc[row_index, 2]
-        questions[row_index]["incorrect answer"] = []
-        category[file_1.iloc[row_index, 0]].append(file_1.iloc[row_index, 2])
+        for i in range(3):
+            # if file_1.iloc[row_index, 0+i*4] not in category:
+            #     category[file_1.iloc[row_index, 0+i*4]] = []
+            questions["questions"].append({})
+            questions["questions"][row_index*3+i]["category"] = str(row[0+i*4]).replace("\u00a0", "")
+            questions["questions"][row_index*3+i]["question"] = str(row[1+i*4]).replace("\u00a0", "")
+            questions["questions"][row_index*3+i]["correct answer"] = str(row[2+i*4]).replace("\u00a0", "")
+            # questions[row_index*3+i]["incorrect answer"] = []
+            # category[file_1.iloc[row_index, 0+i*4]].append(file_1.iloc[row_index, 2+i*4])
 
-    print(questions)
-    print(category)
+    json_string = json.dumps(questions)
+    print(json_string)
+    with open('questions_data.json', 'w') as outfile:
+        json.dump(json_string, outfile)
+    # print(category)
 
 
 if __name__ == "__main__":
